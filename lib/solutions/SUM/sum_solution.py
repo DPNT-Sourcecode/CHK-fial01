9a5abfbe9361bf85a4d318ef2d5d16681e6ec37f
sbx_user1051 <sbx_user1051@169.254.15.113>
2025-09-14 17:24:01 +0000

    Sun Sep 14 17:24:01 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,4 +2,8 @@
 class SumSolution:
     
     def compute(self, x, y):
-        raise NotImplementedError()
+        if x > 100 or x < 0:
+            raise ValueError(f"x={x} value out of bounds")
+        if y > 100 or y < 0:
+            raise ValueError(f"y={y} value out of bounds")
+        return x + y
--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -2,13 +2,15 @@ from lib.solutions.SUM.sum_solution import SumSolution
 import pytest
 
 
-@pytest.mark.parametrize("inputX, inputY,inputZ")
-def test_sum():
-    assert SumSolution().compute(1, 2) == 3
+@pytest.mark.parametrize("input_x, input_y, output", [(1, 2, 3), (47, 22, 69), (0, 12, 12), (71, 44, 115),
+                                                      (7, 100, 107)])
+def test_sum(input_x, input_y, output):
+    assert SumSolution().compute(input_x, input_y) == output
 
 
-def test_sum_out_of_bounds():
+@pytest.mark.parametrize("input_x, input_y", [(-1, 2), (-10, -11), (18, -9), (105, 4), (105, 204), (14, 341)])
+def test_sum_out_of_bounds(input_x, input_y):
     with pytest.raises(ValueError):
-        SumSolution().compute(-1, 1)
+        SumSolution().compute(input_x, input_y)
 
 
