4f38bfeda59de5bfd6448eaa10aa265b42e273df
sbx_user1051 <sbx_user1051@169.254.15.113>
2025-09-14 17:56:24 +0000

    Sun Sep 14 17:56:24 UTC 2025

--- test/solution_tests/HLO/test_HLO_R1.py
+++ test/solution_tests/HLO/test_HLO_R1.py
@@ -3,5 +3,5 @@ from solutions.HLO.hello_solution import HelloSolution
 
 def test_hello():
     output = HelloSolution().hello("Sophie")
-    assert "Sophie" in output
-    assert "hello" in output.lower()
+    assert output == "hello Sophie"
+
