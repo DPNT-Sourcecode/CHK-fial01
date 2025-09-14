abe11410c8d4b36056905274f2b9494f3e37576f
sbx_user1051 <sbx_user1051@169.254.15.113>
2025-09-14 17:18:01 +0000

    Sun Sep 14 17:18:01 UTC 2025

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -1,6 +1,14 @@
 from lib.solutions.SUM.sum_solution import SumSolution
+import pytest
 
 
+@pytest.mark.parametrize("inputX, inputY,inputZ")
 def test_sum():
     assert SumSolution().compute(1, 2) == 3
+
+
+def test_sum_out_of_bounds():
+    with pytest.raises(ValueError):
+        SumSolution().compute(-1, 1)
+
 
