bb49a4627a9cf874b4f1d86b299a9d5260bda82c
sbx_user1051 <sbx_user1051@169.254.62.111>
2025-09-16 11:34:47 +0000

    Tue Sep 16 11:34:47 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -16,15 +16,34 @@ class CheckoutSolution:
         if len(units) == 0:
             return -1
 
-        # noOfA = re.
+        a = get_a(units)
+        b = get_b(units)
+        c = get_c(units)
+        d = get_d(units)
+        if len(a) > 1 or len(b) > 1 or len(c) > 1 or len(d) > 1:
+            return -1
         return 0
 
 
+# brute force
 def get_a(units: list[str]):
     a_reg = r"\d+A"
-    # check for value errors?
-    units_of_a = [int(unit.replace("A", "")) for unit in units if re.match(a_reg, unit)]
-    return units_of_a
+    return [int(unit.replace("A", "")) for unit in units if re.match(a_reg, unit)]
+
+
+def get_b(units: list[str]):
+    reg = r"\d+B"
+    return [int(unit.replace("B", "")) for unit in units if re.match(reg, unit)]
+
+
+def get_c(units: list[str]):
+    reg = r"\d+C"
+    return [int(unit.replace("C", "")) for unit in units if re.match(reg, unit)]
+
+
+def get_d(units: list[str]):
+    reg = r"\d+D"
+    return [int(unit.replace("D", "")) for unit in units if re.match(reg, unit)]
 
 
 def get_units_list(skus: str) -> list[str]:
@@ -32,6 +51,7 @@ def get_units_list(skus: str) -> list[str]:
     units = re.findall(regex, skus)
     print(f"type units = {type(units)}")
     return units
+
 
 
 
