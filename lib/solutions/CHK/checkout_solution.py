264ca7662d61ac6d8d00392b1a5d27cc9c05a984
sbx_user1051 <sbx_user1051@169.254.43.43>
2025-09-21 08:47:44 +0000

    Sun Sep 21 08:47:44 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,61 +15,70 @@ class CheckoutSolution:
         if not isinstance(skus, str):
             return -1
 
-        units: list[str] = get_units_list(skus)
-        if len(units) == 0:
-            return -1
-
-        try:
-            amounts_dict: dict[str, int] = get_amounts(units)
-        except ValueError:
-            return -1
-        return get_cost(amounts_dict)
+        # try:
+        #     amounts_dict: dict[str, int] = get_amounts(units)
+        # except ValueError:
+        #     return -1
+        # return get_cost(amounts_dict)
 
 
-def get_units_list(skus: str) -> list[str]:
-    regex = r"(\d+[ABCD])"
-    units = re.findall(regex, skus)
-    return units
 
 
-def get_amounts(units: list[str]) -> dict[str, int]:
-    a_amounts = get_a_amounts(units)
-    b_amounts = get_b_amounts(units)
-    c_amounts = get_c_amounts(units)
-    d_amounts = get_d_amounts(units)
-    if len(a_amounts) > 1 or len(b_amounts) > 1 or len(c_amounts) > 1 or len(d_amounts) > 1:
-        raise ValueError("duplicate units")
-    a_amount = a_amounts[0] if len(a_amounts) > 0 else 0
-    b_amount = b_amounts[0] if len(b_amounts) > 0 else 0
-    c_amount = c_amounts[0] if len(c_amounts) > 0 else 0
-    d_amount = d_amounts[0] if len(d_amounts) > 0 else 0
-    return dict(A=a_amount, B=b_amount, C=c_amount, D=d_amount)
 
 
-# for a larger list would combine these methods
-def get_a_amounts(units: list[str]) -> list[int]:
-    a_reg = r"\d+A"
-    return [int(unit.replace("A", "")) for unit in units if re.match(a_reg, unit)]
+
+def get_amounts(skus: str) -> dict[str, int]:
+    pass
 
 
-def get_b_amounts(units: list[str]) -> list[int]:
-    reg = r"\d+B"
-    return [int(unit.replace("B", "")) for unit in units if re.match(reg, unit)]
 
 
-def get_c_amounts(units: list[str]) -> list[int]:
-    reg = r"\d+C"
-    return [int(unit.replace("C", "")) for unit in units if re.match(reg, unit)]
 
 
-def get_d_amounts(units: list[str]) -> list[int]:
-    reg = r"\d+D"
-    return [int(unit.replace("D", "")) for unit in units if re.match(reg, unit)]
-
-
-def get_cost(amounts_dict: dict[str, int]) -> int:
-    c_cost: int = amounts_dict["C"] * C_PRICE
-    d_cost: int = amounts_dict["D"] * D_PRICE
-    a_cost = (amounts_dict["A"] // 3) * A_SPECIAL_PRICE + (amounts_dict["A"] % 3) * A_PRICE
-    b_cost = (amounts_dict["B"] // 2) * B_SPECIAL_PRICE + (amounts_dict["B"] % 2) * B_PRICE
-    return a_cost + b_cost + c_cost + d_cost
+# def get_units_list(skus: str) -> list[str]:
+#     regex = r"(\d+[ABCD])"
+#     units = re.findall(regex, skus)
+#     return units
+#
+#
+# def get_amounts(units: list[str]) -> dict[str, int]:
+#     a_amounts = get_a_amounts(units)
+#     b_amounts = get_b_amounts(units)
+#     c_amounts = get_c_amounts(units)
+#     d_amounts = get_d_amounts(units)
+#     if len(a_amounts) > 1 or len(b_amounts) > 1 or len(c_amounts) > 1 or len(d_amounts) > 1:
+#         raise ValueError("duplicate units")
+#     a_amount = a_amounts[0] if len(a_amounts) > 0 else 0
+#     b_amount = b_amounts[0] if len(b_amounts) > 0 else 0
+#     c_amount = c_amounts[0] if len(c_amounts) > 0 else 0
+#     d_amount = d_amounts[0] if len(d_amounts) > 0 else 0
+#     return dict(A=a_amount, B=b_amount, C=c_amount, D=d_amount)
+#
+#
+# # for a larger list would combine these methods
+# def get_a_amounts(units: list[str]) -> list[int]:
+#     a_reg = r"\d+A"
+#     return [int(unit.replace("A", "")) for unit in units if re.match(a_reg, unit)]
+#
+#
+# def get_b_amounts(units: list[str]) -> list[int]:
+#     reg = r"\d+B"
+#     return [int(unit.replace("B", "")) for unit in units if re.match(reg, unit)]
+#
+#
+# def get_c_amounts(units: list[str]) -> list[int]:
+#     reg = r"\d+C"
+#     return [int(unit.replace("C", "")) for unit in units if re.match(reg, unit)]
+#
+#
+# def get_d_amounts(units: list[str]) -> list[int]:
+#     reg = r"\d+D"
+#     return [int(unit.replace("D", "")) for unit in units if re.match(reg, unit)]
+#
+#
+# def get_cost(amounts_dict: dict[str, int]) -> int:
+#     c_cost: int = amounts_dict["C"] * C_PRICE
+#     d_cost: int = amounts_dict["D"] * D_PRICE
+#     a_cost = (amounts_dict["A"] // 3) * A_SPECIAL_PRICE + (amounts_dict["A"] % 3) * A_PRICE
+#     b_cost = (amounts_dict["B"] // 2) * B_SPECIAL_PRICE + (amounts_dict["B"] % 2) * B_PRICE
+#     return a_cost + b_cost + c_cost + d_cost
--- test/solution_tests/CHK/CHK_test.py
+++ test/solution_tests/CHK/CHK_test.py
@@ -1,58 +1,77 @@
 import pytest
 
 from solutions.CHK.checkout_solution import CheckoutSolution
-from solutions.CHK.checkout_solution import get_a_amounts
 from solutions.CHK.checkout_solution import get_amounts
-from solutions.CHK.checkout_solution import get_cost
-from solutions.CHK.checkout_solution import get_units_list
 
 
 def test_get_amounts():
-    result = get_amounts(["14A", "0B", "16C"])
-    assert result == {"A": 14, "B": 0, "C": 16, "D": 0}
+    input = "AABCCDA"
+    expected = {"A": 3, "B": 1, "C": 2, "D": 1}
+    assert get_amounts(input) == expected
 
 
-def test_get_amounts_raises_value_error():
-    with pytest.raises(ValueError):
-        get_amounts(["1A", "2B", "6A"])
 
 
-def test_checkout_missing_quantity():
-    result = CheckoutSolution().checkout("A")
-    assert result == -1
 
 
-def test_checkout_invalid_input():
-    result = CheckoutSolution().checkout(12)
-    assert result == -1
 
 
-def test_checkout_repeated_sku():
-    result = CheckoutSolution().checkout("1A2B6A")
-    assert result == -1
 
 
-def test_get_units_list():
-    assert get_units_list("3A") == ["3A"]
-    assert get_units_list("3A5B") == ["3A", "5B"]
-    assert get_units_list("1A2B6A") == ["1A", "2B", "6A"]
 
 
-def test_get_a():
-    assert get_a_amounts(["1A", "2B", "6A"]) == [1, 6]
 
 
-def test_get_cost():
-    input = {"A": 14, "B": 11, "C": 16, "D": 4}
-    assert get_cost(input) == 1255
 
 
-def test_checkout():
-    input = "14A11B16C4D"
-    assert CheckoutSolution().checkout(input) == 1255
-    scrambled_input = "16C14A4D11B"
-    assert CheckoutSolution().checkout(scrambled_input) == 1255
-    input_without_A = "2B3C5D"
-    assert CheckoutSolution().checkout(input_without_A) == 180
-    input_with_zero_A = "2B3C5D0A"
-    assert CheckoutSolution().checkout(input_with_zero_A) == 180
+
+# def test_get_amounts():
+#     result = get_amounts(["14A", "0B", "16C"])
+#     assert result == {"A": 14, "B": 0, "C": 16, "D": 0}
+#
+#
+# def test_get_amounts_raises_value_error():
+#     with pytest.raises(ValueError):
+#         get_amounts(["1A", "2B", "6A"])
+#
+#
+# def test_checkout_missing_quantity():
+#     result = CheckoutSolution().checkout("A")
+#     assert result == -1
+#
+#
+# def test_checkout_invalid_input():
+#     result = CheckoutSolution().checkout(12)
+#     assert result == -1
+#
+#
+# def test_checkout_repeated_sku():
+#     result = CheckoutSolution().checkout("1A2B6A")
+#     assert result == -1
+#
+#
+# def test_get_units_list():
+#     assert get_units_list("3A") == ["3A"]
+#     assert get_units_list("3A5B") == ["3A", "5B"]
+#     assert get_units_list("1A2B6A") == ["1A", "2B", "6A"]
+#
+#
+# def test_get_a():
+#     assert get_a_amounts(["1A", "2B", "6A"]) == [1, 6]
+#
+#
+# def test_get_cost():
+#     input = {"A": 14, "B": 11, "C": 16, "D": 4}
+#     assert get_cost(input) == 1255
+
+
+# def test_checkout():
+#     input = "14A11B16C4D"
+#     assert CheckoutSolution().checkout(input) == 1255
+#     scrambled_input = "16C14A4D11B"
+#     assert CheckoutSolution().checkout(scrambled_input) == 1255
+#     input_without_A = "2B3C5D"
+#     assert CheckoutSolution().checkout(input_without_A) == 180
+#     input_with_zero_A = "2B3C5D0A"
+#     assert CheckoutSolution().checkout(input_with_zero_A) == 180
+
