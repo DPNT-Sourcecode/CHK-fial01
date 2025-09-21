c82499c24dd301631bcfec3749636c086bab09c3
sbx_user1051 <sbx_user1051@169.254.43.43>
2025-09-21 09:49:32 +0000

    Sun Sep 21 09:49:32 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,7 +7,7 @@ D_PRICE: int = 15
 E_PRICE: int = 40
 A_THREE_PRICE: int = 130  # 3A for 130
 A_FIVE_PRICE: int = 150  # 3A for 150
-B_SPECIAL_PRICE: int = 45   # 2B for 45
+B_SPECIAL_PAIR_PRICE: int = 45   # 2B for 45
 
 
 class CheckoutSolution:
@@ -40,7 +40,7 @@ def get_cost(amounts_dict: dict[str, int]) -> int:
     c_cost: int = amounts_dict["C"] * C_PRICE
     d_cost: int = amounts_dict["D"] * D_PRICE
     a_cost = get_a_cost(amounts_dict["A"])
-    b_cost = (amounts_dict["B"] // 2) * B_SPECIAL_PRICE + (amounts_dict["B"] % 2) * B_PRICE
+    b_cost = (amounts_dict["B"] // 2) * B_SPECIAL_PAIR_PRICE + (amounts_dict["B"] % 2) * B_PRICE
     return a_cost + b_cost + c_cost + d_cost
 
 
@@ -50,8 +50,6 @@ def get_a_cost(no_of_a: int) -> int:
     return calculate_naive_a_cost(no_of_a)
 
 
-# Could there be cases where it's cheaper to break the last 5 into more threes?
-# consider no_of_a = 6. Either 150 + 50 = 200 or 130*2 = 260
 def calculate_naive_a_cost(no_of_a: int) -> int:
     no_of_fives = no_of_a // 5
     remainder_from_fives = no_of_a % 5
@@ -60,5 +58,14 @@ def calculate_naive_a_cost(no_of_a: int) -> int:
     return (no_of_fives * A_FIVE_PRICE +
             no_of_threes_in_remainder * A_THREE_PRICE +
             remainder_of_remainder * A_PRICE)
+
+
+def calculate_naive_b_cost(no_of_b: int, no_of_e: int):
+    no_of_free_b = no_of_e // 2
+    b_to_pay = no_of_b - no_of_free_b
+    pairs_of_b = b_to_pay // 2
+    single_b = b_to_pay % 2
+    return pairs_of_b * B_SPECIAL_PAIR_PRICE + single_b * B_PRICE
+
 
 
